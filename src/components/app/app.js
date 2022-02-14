fa008eeb6051e74b1522712964f1ab911ab72d26
Aikeilol <garena3307@mail.ru>
2022-02-14 20:37:40 +0300

    add state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import './app.css';
 
 
 function App() {
+
+  const data = [
+    { name: 'John C.', salary: 800, increase: false, id: 1, },
+    { name: 'Alex M.', salary: 3000, increase: false, id: 2, },
+    { name: 'Michael J.', salary: 5000, increase: true, id: 3, },
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -14,7 +21,7 @@ function App() {
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
